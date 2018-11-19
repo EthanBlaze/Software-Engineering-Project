ab6011aea7e7482a781595d4e3b39bd4c32ecf64
abbymox <43238209+abbymox@users.noreply.github.com>
2018-11-18 21:35:27 -0600

    Added checks
    Added checks to ensure name does not contain digits/is not blank.

--- teacher.py
+++ teacher.py
@@ -1,7 +1,21 @@
+from inputErrors import NameWithNumbersError
+from inputErrors import NoNameError
+
+
 class Teacher:
     def __init__(self, lname, fname, password):
-        self.lname = lname
-        self.fname = fname
+        try:
+            if any(str.isdigit(c) for c in lname) or any(str.isdigit(c) for c in fname):
+                raise NameWithNumbersError
+            elif not lname or not fname:
+                raise NoNameError
+            else:
+                self.fname = fname
+                self.lname = lname
+        except NameWithNumbersError:
+                return "Name cannot contain anything other than letters."
+        except NoNameError:
+                return "Names cannot be blank."
         self.password = password
 
 
