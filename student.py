b03f204ddf483717001786c9740cd19a87e51e2e
abbymox <43238209+abbymox@users.noreply.github.com>
2018-11-18 21:35:19 -0600

    Added checks
    Added checks to ensure name does not contain digits/is not blank.

--- student.py
+++ student.py
@@ -1,10 +1,25 @@
+from inputErrors import NameWithNumbersError
+from inputErrors import NoNameError
+
+
 class Student:
 
     def __init__(self, lname, fname, id, courses=[]):
-        self.fname = fname
-        self.lname = lname
+        try:
+            if any(str.isdigit(c) for c in lname) or any(str.isdigit(c) for c in fname):
+                raise NameWithNumbersError
+            elif not lname or not fname:
+                raise NoNameError
+            else:
+                self.fname = fname
+                self.lname = lname
+        except NameWithNumbersError:
+            return "Name cannot contain anything other than letters."
+        except NoNameError:
+            return "Names cannot be blank."
         self.id = id
-        self.courses = courses
+        for course in courses:
+            self.addCourse(course)
 
     def addCourse(self, course):
         self.courses.append(course)
